09564580e2057b5b8215afd5caba12ceb5ef0a89
Iker Ramírez <ikergaelramirez31@gmail.com>
2025-05-22 22:21:08 -0600

    Update constantes.py

--- constantes.py
+++ constantes.py
@@ -1,9 +1,8 @@
 import os
 
-# Ruta del logo (ajusta si cambia la imagen o carpeta)
 RUTA_LOGO = os.path.join("imagenes", "escudoITT.png")
 
-# COLORES
+
 NAVBAR_COLOR = "#002E5A"
 TEXT_COLOR = "white"
 BTN_GREEN = "#28a745"
@@ -11,7 +10,6 @@ BTN_RED = "#dc3545"
 BTN_BLUE = "#007bff"
 FONDO_GENERAL = "#D9D9D9"
 
-# FUENTES (si luego quieres unificarlas)
 FUENTE_TITULO = ("Arial", 16, "bold")
 FUENTE_NORMAL = ("Segoe UI", 10)
 FUENTE_BOTON = ("Segoe UI", 10, "bold")
